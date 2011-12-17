6ff287c4fceb3c064822253a768c73d7a4fcec68
Ursin <ursin.brunner@gmail.com>
2011-12-17 02:47:58 +0100

    file import works now for .doc and .docx word files. Just zip them and use the upload

--- src/main/java/com/zuehlke/lab/service/importer/ImporterService.java
+++ src/main/java/com/zuehlke/lab/service/importer/ImporterService.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import javax.ejb.Stateless;
@@ -21,6 +20,8 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.apache.commons.io.IOUtils;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
@@ -53,19 +54,31 @@ public class ImporterService {
             outputStream = new ByteArrayOutputStream();
             IOUtils.copy(wordFilesAsStream, outputStream);
             try {
-                String rawData = getWordDocContentByRawData(outputStream.toByteArray());
+                
                 Person p = new Person();
-                p.setRawdata(rawData);
+                String filePrefix = "";
 
                 try {
-                    filterFirstnameLastNameByFilename(entry.getName(), p);
+                    filePrefix = filterFirstnameLastNameByFilename(entry.getName(), p);
                 } catch (IllegalArgumentException ex) {
                     System.out.println(ex.getMessage());
                 }
+                
+                String rawData = "";
+                if(filePrefix.equals("doc"))
+                    rawData = getWordDocContentByRawData(outputStream.toByteArray());
+                else if(filePrefix.equals("docx"))
+                    rawData = getWordDocxContentByRawData(outputStream.toByteArray());
+                else
+                    throw new IllegalArgumentException("Unknown file-prefix ["+filePrefix+"] !");
+                
+                p.setRawdata(rawData);
+
                 personWithRawData.add(p);
-            }
-            catch(Exception ex){
-                System.out.println("Could not get rawdata for ["+entry.getName()+"]. Reason was:"+ex.getStackTrace());
+                
+                System.out.println("Data for Person ["+p.getLastname() +" "+p.getFirstname()+"] successfully imported");
+            } catch (Exception ex) {
+                System.out.println("Could not get rawdata for [" + entry.getName() + "]. Reason was:" + ex.getStackTrace());
             }
 
 
@@ -75,19 +88,34 @@ public class ImporterService {
         return personWithRawData;
     }
 
-    private String getWordDocContentByRawData(byte[] data) throws IOException {
+    private String getWordDocxContentByRawData(byte[] data) throws IOException {
         XWPFDocument doc = new XWPFDocument(new ByteArrayInputStream(data));
 
         XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
         return extractor.getText();
     }
 
-    private void filterFirstnameLastNameByFilename(String filename, Person storeInThis) throws IllegalArgumentException {
+    private String getWordDocContentByRawData(byte[] data) throws IOException {
+        HWPFDocument doc = new HWPFDocument(new ByteArrayInputStream(data));
+
+        WordExtractor extractor = new WordExtractor(doc);
+        return extractor.getText();
+    }
+
+    /**
+     * 
+     * @param filename
+     * @param storeInThis
+     * @return the file prefix (doc or docx)
+     * @throws IllegalArgumentException 
+     */
+    private String filterFirstnameLastNameByFilename(String filename, Person storeInThis) throws IllegalArgumentException {
 
         try {
             String[] lastnameFirstName = filename.split("(_|\\.)");
             storeInThis.setLastname(lastnameFirstName[0]);
             storeInThis.setFirstname(lastnameFirstName[1]);
+            return lastnameFirstName[2];
         } catch (NoSuchElementException e) {
             throw new IllegalArgumentException("filename [" + filename + "] seems not to contain a vaild name.", e);
         }
