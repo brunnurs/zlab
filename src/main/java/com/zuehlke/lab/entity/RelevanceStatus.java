9ea56d5bd2bdcd84acb957b65d9e7e74a0ca46ba
Bolliger Michael <bolliger.michi@gmail.com>
2012-01-02 20:03:56 -0500

    relevance word service

---
+++ src/main/java/com/zuehlke/lab/entity/RelevanceStatus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zuehlke.lab.entity;
+
+/**
+ *
+ * @author user
+ */
+public enum RelevanceStatus {
+    
+    BLACKLISTED, WITHLISTED, AUTO_BLACKLISTED, AUTO_WITHLISTED
+    
+}
