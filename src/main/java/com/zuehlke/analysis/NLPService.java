d80c16c0608708010ae766cd9097c915e69a4028
user <user@ubuntu.localdomain>
2012-01-06 14:21:02 -0500

    Extended service for named entities.

--- src/main/java/com/zuehlke/analysis/NLPService.java
+++ src/main/java/com/zuehlke/analysis/NLPService.java
@@ -21,43 +21,70 @@ import org.apache.commons.lang3.tuple.Pair;
 @Stateless
 @LocalBean
 public class NLPService {
-    
+
     @EJB
     private POSTaggingService posTaggingService;
-    
     @EJB
     private LanguageDetectionService languageDetectionService;
-    
+
     public Map<String, MutableInt> extractTerms(String text) {
         List<Pair<String, String>> taggedWords = Collections.EMPTY_LIST;
-        
+
         Language language = languageDetectionService.getLanguage(text);
-        
-        if(language == Language.GERMAN){
+
+        if (language == Language.GERMAN) {
             taggedWords = posTaggingService.tagGermanText(text);
         }
-        if(language == Language.ENGLISH){
+        if (language == Language.ENGLISH) {
             taggedWords = posTaggingService.tagEnglishText(text);
         }
-        
-        Map<String, MutableInt> nouns = new HashMap<String, MutableInt>();
-        for(Pair<String, String> taggedWord : taggedWords){
-            if(taggedWord.getRight().equals("NN")){
-                if(nouns.containsKey(taggedWord.getLeft())){
-                    nouns.get(taggedWord.getLeft()).increment();
-                }else{
-                    nouns.put(taggedWord.getLeft(), new MutableInt(1));
-                }
+
+        Map<String, MutableInt> terms = new HashMap<String, MutableInt>();
+
+        while (!taggedWords.isEmpty()) {
+            String term = getNextTerm(taggedWords);
+            if (terms.containsKey(term)) {
+                terms.get(term).increment();
+            } else {
+                terms.put(term, new MutableInt(1));
             }
         }
-        return nouns;
+
+        return terms;
     }
-    
-    public void setPosTaggingService(POSTaggingService posTaggingService){
+
+    private String getNextTerm(List<Pair<String, String>> taggedWords) {
+        while (!taggedWords.isEmpty()) {
+            Pair<String, String> taggedWord = taggedWords.remove(0);
+            if (taggedWord.getRight().equals("NN")) {
+                return taggedWord.getLeft();
+            } else if (taggedWord.getRight().equals("NE")) {
+                return extractNamedEntity(taggedWord.getLeft(), taggedWords);
+            }
+        }
+        return "";
+    }
+
+    private String extractNamedEntity(String firstWord, List<Pair<String, String>> taggedWords) {
+        StringBuilder namedEntity = new StringBuilder(firstWord);
+        while (!taggedWords.isEmpty()) {
+            Pair<String, String> taggedWord = taggedWords.remove(0);
+            if (taggedWord.getRight().equals("NE")) {
+                namedEntity.append(" ");
+                namedEntity.append(taggedWord.getLeft());
+            } else {
+                break;
+            }
+        }
+
+        return namedEntity.toString();
+    }
+
+    public void setPosTaggingService(POSTaggingService posTaggingService) {
         this.posTaggingService = posTaggingService;
     }
-    
-    public void setLanguageDetectionService(LanguageDetectionService languageDetectionService){
+
+    public void setLanguageDetectionService(LanguageDetectionService languageDetectionService) {
         this.languageDetectionService = languageDetectionService;
     }
 }
